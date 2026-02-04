7b9f0837003f5c9fd17c4ac9342ce3d206fdd66a
xoyoc <xoyoc_l2@hotmail.com>
2026-02-04 11:10:07 -0600

    chore: Update settings

--- checador/settings.py
+++ checador/settings.py
@@ -187,6 +187,16 @@ DEFAULT_FROM_EMAIL = 'checadorKasu@transportekasu.com.mx'
 # Seguridad
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['https://*.ondigitalocean.app'])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['https://checador-kasu-app-xddvb.ondigitalocean.app'])
+# CORS Settings
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+    'https://checador-kasu-app-xddvb.ondigitalocean.app',
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
